92d8e4be58d91c5381ac10442c717ec7a46fa246
Daniel Alberto Posada Murillo <dposad21@eafit.edu.co>
2024-02-08 11:40:27 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home (request):
-    return render(request,'home.html',{'name':'Daniel Alberto Posada Murillo'})
+    return render(request,'home.html',{'name':'Daniel Posada'})
 
 def about (request):   
     return HttpResponse('<h1>Page about</h1>')
